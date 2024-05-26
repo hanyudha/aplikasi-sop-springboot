cb6ec1c1382e5370b0dff4d16dc1b2049dd56968
wandy.hanyudha <hanyudha@uinjkt.ac.id>
2024-05-26 11:20:55 +0700

    Initial commit

---
+++ src/main/java/com/example/aplikasisopspringboot/AplikasiSopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplikasisopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiSopSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikasiSopSpringbootApplication.class, args);
+    }
+
+}
