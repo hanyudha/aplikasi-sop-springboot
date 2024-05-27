f415cabefa59d34e77b920f6fa9662c356e79ac5
Wandy <hanyudha@gmail.com>
2024-05-27 16:17:53 +0700

    fix sop page, entity, view

--- src/main/java/com/example/aplikasisopspringboot/dto/SopDto.java
+++ src/main/java/com/example/aplikasisopspringboot/dto/SopDto.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SopDto {
     private String id;
     private String nomorSop;
+    private LocalDateTime tglPembuatan;
+    private LocalDateTime tglRevisi;
+    private LocalDateTime tglEfektif;
+    private String disahkanOleh;
     private String namaSop;
 }
--- src/main/java/com/example/aplikasisopspringboot/entity/Sop.java
+++ src/main/java/com/example/aplikasisopspringboot/entity/Sop.java
@@ -4,6 +4,12 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(schema = "public", name = "sop")
@@ -19,6 +25,22 @@ public class Sop {
 
     @Column(name = "nomor_sop", columnDefinition = "text")
     private String nomorSop;
+
+    @CreatedDate
+    @Column(name = "created_at", length = 30, updatable = false)
+    private LocalDate tglPembuatan;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDate tglRevisi;
+
+
+    @Column(name = "efektif_at")
+    private LocalDate tglEfektif;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String disahkanOleh;
 
     @Column(name = "nama_sop", columnDefinition = "text")
     private String namaSop;
